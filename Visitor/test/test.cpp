3373ce7ca3a13fb51191abd32a963966aa95530c
Taketo Sasaki <sasaki.taketo@gmail.com>
2014-08-21 22:29:01 +1000

    update makefile. added unittest

--- Visitor/test/test.cpp
+++ Visitor/test/test.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include "Visitor.hh"
+#include "Observer.hh"
 
 int Increment(int val) { return val + 1; }
 int Decrement(int val) { return val - 1; }
@@ -6,6 +8,12 @@ int Decrement(int val) { return val - 1; }
 TEST(HelloGoogleTest, Increment)
 {
   EXPECT_EQ(2, Increment(1));
+    NumberGenerator n;
+    TestObserver t;
+    n.addObserver(&t);
+    ExecObserver e;
+    n.addObserver(&e);
+    n.execute();
 }
 
 TEST(HelloGoogleTest, Decrement)
